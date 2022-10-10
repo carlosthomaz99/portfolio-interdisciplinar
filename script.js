c3ff620f1326cc8fc0e34440302050a8fdab790e
carlosthomaz99 <carlosthomaz99@gmail.com>
2022-10-10 16:31:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const NAME = "CarlosThomaz";
+const PASSWORD = "12345";
+
+function validateForm() {
+    let name = document.forms["form"]["fname"].value;
+    let pass = document.forms["form"]["fpass"].value;
+
+    if (name == "") {
+        alert("Preencha todos os campos");
+        return false;
+    }
+
+    if (pass == "") {
+        alert("Preencha todos os campos");
+        return false;
+    }
+
+    return validateCredential(name, pass);
+
+}
+
+function validateCredential(name, pass) {
+
+    if(name != NAME) {
+        alert("usuário ou senha incorretos");
+        return false;
+    }
+
+    if(pass != PASSWORD) {
+        alert("usuário ou senha incorretos");
+        return false;
+    }
+
+    turnOnGreetings();
+
+    return true;
+
+}
+
+function turnOnGreetings() {
+    const loginForm = document.getElementById("loginForm");
+    loginForm.style.opacity = 0;
+
+    const greetings = document.getElementById("greetings");
+    greetings.style.opacity = 1;
+}
